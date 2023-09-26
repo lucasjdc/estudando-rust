554ee71f72a33f53f05058298aecce839b4a86e4
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2023-09-26 12:23:13 -0300

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,14 +1,24 @@
+const PI:f32 = 3.14;
+static mut VARIAVEL_GLOBAL:u8 = 1;
+
 fn main(){
-   let variavel:i32 = 127;
-   println!("variavel = {}, tamanho  = {} bytes", variavel, std::mem::size_of_val(&variavel));
+   println!("PI ≈ {}", PI);
+   
+   unsafe {
+      println!("tamanho da variável global é {} bytes.", std::mem::size_of_val(&VARIAVEL_GLOBAL));
+   }
+   
+
+   let variavel:i8 = 127;
+   println!("variável = {}, tamanho  = {} bytes", variavel, std::mem::size_of_val(&variavel));
 
    let decimal:f32 = 2.5;
-   println!("variavel = {}", decimal);
+   println!("variável = {}", decimal);
    
    let booleana:bool = true;
    println!("Booleana = {}, tamanho booleana = {}", booleana,  std::mem::size_of_val(&booleana));
 
-   let letra:char = 'C';
+   let letra:char = 'R';
    println!("Tamanho do char = {}", std::mem::size_of_val(&letra));
 
 }
